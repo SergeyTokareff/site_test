023a5b59bda83cb823f2e60657cf86c7656c0553
SergeyTokareff <sergey.v.tokareff@gmail.com>
2016-03-23 22:12:46 +0200

    ffff

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once "header.php"; ?>
+				
+				<div class="main_left">
+
+				</div>
+				<div class="main_right">
+					<?php require_once "authentication.php"; ?>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
